5c853a7f2d65279dbbd2baa74700e73e41063b82
Doug Belshaw <dajbelshaw@users.noreply.github.com>
2026-02-21 16:36:52 +0100

    Delete instructions directory

--- instructions/index.ts
+++
@@ -1,5 +0,0 @@
-export { createAuthRouter } from './routes';
-export { requireAuth } from './middleware';
-export { generateUniqueEmojiId, isValidEmojiId } from './emoji-id';
-export { validateSession } from './session';
-export type { Session } from './session';
